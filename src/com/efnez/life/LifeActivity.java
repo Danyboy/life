9956d0ee9f552f4195615f14e50c573829a88c23
Danil Mikhaylov <danil@etersoft.ru>
2013-09-16 08:53:19 +0400

    another start

--- src/com/efnez/life/LifeActivity.java
+++ src/com/efnez/life/LifeActivity.java
@@ -1,6 +1,6 @@
 package com.efnez.life;
 
-import android.R;
+//import android.R;
 import android.app.Activity;
 import android.os.Bundle;
 
@@ -11,6 +11,6 @@ public class LifeActivity extends Activity
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-//        setContentView(R.layout.main);
+        setContentView(R.layout.main);
     }
 }
